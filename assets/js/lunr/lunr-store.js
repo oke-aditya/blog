e7be680fa2de897801ae3854f096a433ba39fa5a
oke-aditya <oke-aditya@users.noreply.github.com>
2021-02-06 07:52:13 +0000

    jekyll build from Action dc0f9837180a8c2773ef0658c3c32e5d2c9ae79a

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Challenges with Machine Learning Projects",
+        "excerpt":"Introduction Machine Learning Projects are hard to make. Maintaining them is even much harder. Code becomes complex and end of month or year, it becomes impossible to understand it. But Why do projects become spaghetti code ? Lack of project structure Projects that lack structure intially later become a mess....","categories": [],
+        "tags": ["Machine Learning"],
+        "url": "/blog/challenges-ml-projects/",
+        "teaser": null
+      },{
+        "title": "Jekyll For Blogging",
+        "excerpt":"How to build And Deploy a Jekyll Blog ?   Building a blog page is cumbersome, there are lot of features to worry about.   In Progress !! Will complete this soon .  ","categories": [],
+        "tags": ["Blogging"],
+        "url": "/blog/make-a-blog/",
+        "teaser": null
+      }]
